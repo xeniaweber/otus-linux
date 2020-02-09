5ad9dec121f243545965d11ddfe5e56ef49bb39c
xeniaweber <ksenia94weber@gmail.com>
2020-02-10 00:53:50 +0300

    Create raid5.sh

---
+++ hw2/step1/raid5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mdadm --zero-superblock --force /dev/sd{b,c,d,e}
+mdadm --zero-superblock --force /dev/sd{b,c,d,e}
+mkdir /erc/mdadm
+echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
+parted -s /dev/md0 mklabel gpt
+parted /dev/md0 mkpart primary ext4 0% 20%
+parted /dev/md0 mkpart primary ext4 20% 40%
+parted /dev/md0 mkpart primary ext4 40% 60%
+parted /dev/md0 mkpart primary ext4 60% 80%
+parted /dev/md0 mkpart primary ext4 80% 100%
+for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
+mkdir -p /raid/part{1,2,3,4,5}
+for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
+lsblk
